4fe7601704f389a82137e1539e382242f9e5f626
Dong T. Pham <Dong.TrongPham@coxautoinc.com>
2021-05-28 12:53:51 +0700

    push

--- app/src/main/java/com/dongpt/demoproject/MainActivity.kt
+++ app/src/main/java/com/dongpt/demoproject/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dongpt.demoproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dongpt.demoproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnToolbarNavigation.setOnClickListener {
+            
+        }
     }
 }
---
+++ app/src/main/java/com/dongpt/demoproject/toolbarNavigation/ToolbarNavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.dongpt.demoproject.toolbarNavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dongpt.demoproject.R
+
+class ToolbarNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toolbar_navigation)
+    }
+}
